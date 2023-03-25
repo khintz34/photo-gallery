27c9b9963c40626757c188d418205e042d21d8fc
Kevin Hintz <khintz34@gmail.com>
2023-03-25 11:30:51 -0500

    gallery images

---
+++ src/assets/PhotoArray.jsx
@@ -0,0 +1,165 @@
+import bridge from "../assets/photoLib/bridgeV.jpg";
+import campingH from "../assets/photoLib/campingH.jpg";
+import campingH2 from "../assets/photoLib/campingH2.jpg";
+import campingV from "../assets/photoLib/campingV.jpg";
+import charlestonH from "../assets/photoLib/charlestonH.jpg";
+import charlestonV from "../assets/photoLib/charlestonV.jpg";
+import coputerH from "../assets/photoLib/coputerH.jpg";
+import fireH from "../assets/photoLib/fireH.jpg";
+import fireV from "../assets/photoLib/fireV.jpg";
+import hockeyH from "../assets/photoLib/hockeyH.jpg";
+import lighthouseH from "../assets/photoLib/lighthouseH.jpg";
+import mountainV from "../assets/photoLib/mountainV.jpg";
+import skiH from "../assets/photoLib/skiH.jpg";
+import stadiumH from "../assets/photoLib/stadiumH.jpg";
+import sunrideH from "../assets/photoLib/sunrideH.jpg";
+import sunriseV from "../assets/photoLib/sunriseV.jpg";
+import surfV from "../assets/photoLib/surfV.jpg";
+import waterH from "../assets/photoLib/waterH.jpg";
+
+export const PhotoArray = [
+  {
+    name: "Bridge",
+    image: bridge,
+    date: new Date("2020, 10, 12"),
+    album: "",
+    people: [],
+    places: ["charleston", "south carolina"],
+  },
+  {
+    name: "CampingH",
+    image: campingH,
+    date: new Date("2021, 5, 12"),
+    album: "Camping",
+    people: [],
+    places: ["charleston", "south carolina"],
+  },
+  {
+    name: "CampingH2",
+    image: campingH2,
+    date: new Date("2022, 7, 15"),
+    album: "Camping",
+    people: [],
+    places: ["minnesota"],
+  },
+  {
+    name: "CampingV",
+    image: campingV,
+    date: new Date("2020, 1, 20"),
+    album: "Camping",
+    people: [],
+    places: ["chaska", "minnesota"],
+  },
+  {
+    name: "charlestonH",
+    image: charlestonH,
+    date: new Date("2021, 3, 29"),
+    album: "Charleston",
+    people: [],
+    places: ["charleston", "south carolina"],
+  },
+  {
+    name: "charlestonV",
+    image: charlestonV,
+    date: new Date("2021, 3, 19"),
+    album: "Charleston",
+    people: [],
+    places: ["charleston", "south carolina"],
+  },
+  {
+    name: "computer",
+    image: coputerH,
+    date: new Date("2019, 1, 19"),
+    album: "",
+    people: [],
+    places: [],
+  },
+  {
+    name: "fireH",
+    image: fireH,
+    date: new Date("2023, 1, 19"),
+    album: "",
+    people: [],
+    places: [],
+  },
+  {
+    name: "fireV",
+    image: fireV,
+    date: new Date("2021, 7, 12"),
+    album: "",
+    people: [],
+    places: [],
+  },
+  {
+    name: "hockeyH",
+    image: hockeyH,
+    date: new Date("2021, 12, 25"),
+    album: "Sports",
+    people: ["Hadley"],
+    places: [],
+  },
+  {
+    name: "mountainV",
+    image: mountainV,
+    date: new Date("2018, 11, 17"),
+    album: "",
+    people: [],
+    places: ["switzerland"],
+  },
+  {
+    name: "lighthouseH",
+    image: lighthouseH,
+    date: new Date("2021, 4, 1"),
+    album: "",
+    people: [],
+    places: ["south carolina"],
+  },
+  {
+    name: "skiH",
+    image: skiH,
+    date: new Date("2021, 2, 2"),
+    album: "Sports",
+    people: ["Ben"],
+    places: ["switzerland"],
+  },
+  {
+    name: "stadiumH",
+    image: stadiumH,
+    date: new Date("2017, 6, 8"),
+    album: "Sports",
+    people: [],
+    places: [],
+  },
+  {
+    name: "sunrideH",
+    image: sunrideH,
+    date: new Date("2018, 5, 20"),
+    album: "",
+    people: [],
+    places: ["Lake Valentine"],
+  },
+  {
+    name: "sunriseV",
+    image: sunriseV,
+    date: new Date("2014, 6, 2"),
+    album: "",
+    people: [],
+    places: ["Pretty Lake"],
+  },
+  {
+    name: "surfV",
+    image: surfV,
+    date: new Date("2020, 10, 3"),
+    album: "Sports",
+    people: ["Tim", "Josh"],
+    places: ["south carolina"],
+  },
+  {
+    name: "waterH",
+    image: waterH,
+    date: new Date("2021, 7, 4"),
+    album: "Sports",
+    people: ["Jimmy"],
+    places: ["hawaii"],
+  },
+];
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,18 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { PhotoArray } from "../../assets/PhotoArray";
 import "./Gallery.css";
 
 const Gallery = () => {
+  const sortedPhotoArray = PhotoArray.sort(function (a, b) {
+    return new Date(a.date) - new Date(b.date);
+  });
+  const [photoList, setPhotoList] = useState(PhotoArray);
+
+  console.log(photoList[0].date);
+
   return (
     <div id="galleryContainer">
-      {(() => {
-        let dots = [];
-        for (let i = 0; i < 16; i++) {
-          console.log(i);
-          dots.push(<div>{i}</div>);
-        }
-        return dots;
-      })()}
+      <div id="dateContainer">Date</div>
+      <div id="galleryMain">
+        {(() => {
+          let pics = [];
+          for (let i = 0; i < PhotoArray.length; i++) {
+            pics.push(
+              <div className="picContainer" key={`${photoList[i].name}-image`}>
+                <img
+                  src={photoList[i].image}
+                  alt={photoList[i].name}
+                  className="photoArrayImage"
+                />
+              </div>
+            );
+          }
+          return pics;
+        })()}
+      </div>
     </div>
   );
 };
